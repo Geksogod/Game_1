ff0d3cc89ccfc546dc70b93b866fc2ee7a81c54e
Geksogod <41061522+Geksogod@users.noreply.github.com>
2019-02-11 22:08:09 +0200

    Birza_Full

--- New post/Assets/Birza_Sc.cs
+++ New post/Assets/Birza_Sc.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class Birza_Sc : MonoBehaviour
 {
     public int value;
     public string name_b;
+    private int chislo_do;
    
     // Start is called before the first frame update
     void Start()
@@ -18,13 +20,23 @@ public class Birza_Sc : MonoBehaviour
     
     private int Rand(int value_do)
     {
-        return value_do + Random.Range(-20,20);
+        return UnityEngine.Random.Range(value,value*2);
     }
 
     public void End()
     {
-        GetComponentsInChildren<Text>()[2].text = Rand(value).ToString();
-        value = Rand(value);
+        chislo_do = Rand(value);
+
+        if (chislo_do > Convert.ToInt32(GetComponentsInChildren<Text>()[2].text))
+            GetComponentsInChildren<Text>()[2].color = Color.green;
+        else if (chislo_do < Convert.ToInt32(GetComponentsInChildren<Text>()[2].text))
+            GetComponentsInChildren<Text>()[2].color = Color.red;
+        else            
+            GetComponentsInChildren<Text>()[2].color = Color.gray;
+
+
+        GetComponentsInChildren<Text>()[2].text = chislo_do.ToString();
+        
         
     }
 }
--- New post/Assets/End_Day_Sc.cs
+++ New post/Assets/End_Day_Sc.cs
@@ -7,7 +7,15 @@ public class End_Day_Sc : MonoBehaviour
     private Info info;
     public int e_Add;
     public int hea_Add;
-    public GameObject birza;
+    public GameObject birza_1;
+
+    public GameObject birza_2;
+
+    public GameObject birza_3;
+
+    public GameObject birza_4;
+
+    public GameObject birza_5;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,6 +31,10 @@ public class End_Day_Sc : MonoBehaviour
     {
         info.energy_int += e_Add ;
         info.health_int += hea_Add;
-        birza.GetComponent<Birza_Sc>().End();
+        birza_1.GetComponent<Birza_Sc>().End();
+        birza_2.GetComponent<Birza_Sc>().End();
+        birza_3.GetComponent<Birza_Sc>().End();
+        birza_4.GetComponent<Birza_Sc>().End();
+        birza_5.GetComponent<Birza_Sc>().End();
     }
 }
