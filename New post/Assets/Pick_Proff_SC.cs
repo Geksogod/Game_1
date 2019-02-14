84883aec8133fa3be3dc4dc44ceea4ff2d928822
Geksogod <41061522+Geksogod@users.noreply.github.com>
2019-02-14 21:22:11 +0200

    gogog

---
+++ New post/Assets/Pick_Proff_SC.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pick_Proff_SC : MonoBehaviour
+{
+    public int need_IQ;
+    public int need_Power;
+    public int number=0;
+    public int value = -1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ New post/Assets/Working.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Working : MonoBehaviour
+{
+    private int progress;
+    private int value;
+    public int finish_value;
+
+    public Proffesions_sc proff;
+
+
+    void Start()
+    {
+        this.gameObject.GetComponentsInChildren<Image>()[1].color = Color.grey;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.GetComponentsInChildren<Text>()[2].text = progress.ToString() + " %";
+        this.gameObject.GetComponentsInChildren<Text>()[1].text = value.ToString() + "/" + finish_value;
+        if(value >= finish_value)
+        {
+            this.gameObject.GetComponentsInChildren<Image>()[1].color = Color.white;
+        }
+    }
+
+    public void Click()
+    {
+        progress++;
+        progress++;
+        if (progress >= 100)
+        {
+            progress = 0;
+            value++;
+        }
+    }
+    public void Click_Upgrade()
+    {
+        if(this.gameObject.GetComponentsInChildren<Image>()[1].color == Color.white)
+        {
+            finish_value = finish_value * 4;
+            value = 0;
+            progress = 0;
+            this.gameObject.GetComponentsInChildren<Image>()[1].color = Color.grey;
+        }
+    }
+}
